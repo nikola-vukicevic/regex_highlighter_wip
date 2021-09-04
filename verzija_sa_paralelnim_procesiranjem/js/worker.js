9179bc0f06af8e50cd30c19919c41f4a3dc7acc7
Nikola Vukićević <39658013+nikola-vukicevic@users.noreply.github.com>
2021-09-04 10:18:50 +0200

    Add files via upload

---
+++ verzija_sa_paralelnim_procesiranjem/js/worker.js
@@ -0,0 +1,37 @@
+/* -------------------------------------------------------------------------- */
+// Copyright (c) 2021. Nikola Vukićević
+/* -------------------------------------------------------------------------- */
+
+importScripts('analiza_izraza.js')
+importScripts('highlighter_funkcije.js')
+importScripts('highlighter_funkcije_regex.js')
+importScripts('definicije_jezika.js')
+//importScripts('obradaBlokova.js')
+//importScripts('highlighter.js')
+
+let zauzet = false;
+
+onmessage = e => {
+	if(zauzet) return;
+
+	zauzet = true;
+
+	let i_primljeno      = e.data.i_poslato;
+	let ajDi             = e.data.id;
+	//console.log(ajDi)
+	let tekst            = e.data.tekst;
+	let definicijaJezika = mapaKlasa.get(e.data.definicijaJezika);
+	let rezim            = e.data.rezim;
+
+	let rez = obradaKodaWorker(tekst, definicijaJezika, rezim);
+	
+	postMessage(
+		{
+			"i_primljeno": i_primljeno ,
+			"id":          ajDi        ,
+			"listaTokena": rez
+		}
+	)
+
+	zauzet = false;	
+}
